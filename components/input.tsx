8f11c9f9eb8e94513a465e23d7e628056407343e
zadragon <mase0804@naver.com>
2022-03-23 23:07:45 +0900

    add file

--- components/input.tsx
+++ components/input.tsx
@@ -1,27 +1,72 @@
-import {cls} from "../libs/utils";
+import type { UseFormRegisterReturn } from "react-hook-form";
 
-interface ButtonProps {
-    large?: boolean;
-    text: string;
-
-    [key: string]: any;
+interface InputProps {
+    label: string;
+    name: string;
+    kind?: "text" | "phone" | "price";
+    type: string;
+    register: UseFormRegisterReturn;
+    required: boolean;
 }
 
-export default function Button({
-                                   large = false,
-                                   onClick,
-                                   text,
-                                   ...rest
-                               }: ButtonProps) {
+export default function Input({
+                                  label,
+                                  name,
+                                  kind = "text",
+                                  register,
+                                  type,
+                                  required,
+                              }: InputProps) {
     return (
-        <button
-            {...rest}
-            className={cls(
-                "w-full bg-orange-500 hover:bg-orange-600 text-white  px-4 border border-transparent rounded-md shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none",
-                large ? "py-3 text-base" : "py-2 text-sm "
-            )}
-        >
-            {text}
-        </button>
+        <div>
+            <label
+                className="mb-1 block text-sm font-medium text-gray-700"
+                htmlFor={name}
+            >
+                {label}
+            </label>
+            {kind === "text" ? (
+                <div className="rounded-md relative flex  items-center shadow-sm">
+                    <input
+                        id={name}
+                        required={required}
+                        {...register}
+                        type={type}
+                        className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+                    />
+                </div>
+            ) : null}
+            {kind === "price" ? (
+                <div className="rounded-md relative flex  items-center shadow-sm">
+                    <div className="absolute left-0 pointer-events-none pl-3 flex items-center justify-center">
+                        <span className="text-gray-500 text-sm">$</span>
+                    </div>
+                    <input
+                        id={name}
+                        required={required}
+                        {...register}
+                        type={type}
+                        className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+                    />
+                    <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
+                        <span className="text-gray-500">KRW</span>
+                    </div>
+                </div>
+            ) : null}
+            {kind === "phone" ? (
+                <div className="flex rounded-md shadow-sm">
+          <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">
+            +82
+          </span>
+                    <input
+                        id={name}
+                        required={required}
+                        {...register}
+                        type={type}
+                        className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+                    />
+                </div>
+            ) : null}
+        </div>
     );
 }
---
+++ pages/api/user/enter.tsx
@@ -0,0 +1,9 @@
+import {NextApiRequest, NextApiResponse} from "next";
+
+export default function handler(
+    req:NextApiRequest,
+    res:NextApiResponse
+){
+    console.log(req.body)
+    res.status(200).end();
+}
