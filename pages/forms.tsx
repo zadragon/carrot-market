6715546e801d0872055d1deee595a06d5c8841f1
zadragon <mase0804@naver.com>
2022-03-23 22:11:00 +0900

    update today

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,26 +1,47 @@
 import {useState} from "react";
-import {useForm} from "react-hook-form";
+import {FieldErrors, useForm} from "react-hook-form";
+
+interface LoginForm {
+    username: string;
+    password: string;
+    email: string;
+}
 
 export default function Forms() {
-    const {register, watch} = useForm();
-    console.log(watch())
+    const {register, handleSubmit} = useForm<LoginForm>();
+    const onValid = (data:LoginForm) => {
+        console.log('inm here')
+    }
+    const onInvalid = (errors: FieldErrors) => {
+        console.log(errors)
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit(onValid, onInvalid)}>
             <input
-                {...register("usename")}
+                {...register("usename", {
+                    required: "Username is required",
+                    minLength: {
+                        message:"the username should be longer than 5 chars",
+                        value:5
+                    }
+                })}
                 type="text"
                 placeholder="Username"
                 required
             />
             <input
-                {...register("email")}
+                {...register("email", {
+                    required: true
+                })}
                 type="email"
                 placeholder="Email"
                 required
             />
 
             <input
-                {...register("password")}
+                {...register("password", {
+                    required: true
+                })}
                 type="password"
                 placeholder="Password"
                 required
