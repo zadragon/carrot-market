2e7d28eb88253b5e44e74d9eb43d8744a62fc0b6
zadragon <mase0804@naver.com>
2022-04-07 22:52:18 +0900

    update

--- pages/api/user/enter.tsx
+++ pages/api/user/enter.tsx
@@ -1,23 +1,58 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import client from "@/libs/server/client";
-import withHandler from "@/libs/server/withHandler";
+import withHandler from "@libs/server/withHandler";
+import client from "@libs/server/client";
 
-async function handler(req: NextApiRequest,res: NextApiResponse) {
-    const {phone, email} = req.body;
-    if(email){
-        const user = await client.user.findUnizque({
-            where:{
-                email,
-            }
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { phone, email } = req.body;
+    const payload = phone ? { phone: +phone } : { email };
+    const user = await client.user.upsert({
+        where: {
+            ...payload,
+        },
+        create: {
+            name: "Anonymous",
+            ...payload,
+        },
+        update: {},
+    });
+    console.log(user);
+    /* if (email) {
+      user = await client.user.findUnique({
+        where: {
+          email,
+        },
+      });
+      if (user) console.log("found it.");
+      if (!user) {
+        console.log("Did not find. Will create.");
+        user = await client.user.create({
+          data: {
+            name: "Anonymous",
+            email,
+          },
         });
-        if(!user){
-            await client.user.create({
-
-            })
-        }
+      }
+      console.log(user);
     }
-
-    res.status(200).end()
+    if (phone) {
+      user = await client.user.findUnique({
+        where: {
+          phone: +phone,
+        },
+      });
+      if (user) console.log("found it.");
+      if (!user) {
+        console.log("Did not find. Will create.");
+        user = await client.user.create({
+          data: {
+            name: "Anonymous",
+            phone: +phone,
+          },
+        });
+      }
+      console.log(user);
+    } */
+    return res.status(200).end();
 }
 
-export default withHandler("POST",handler)
+export default withHandler("POST", handler);
