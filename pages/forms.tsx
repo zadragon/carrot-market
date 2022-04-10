ca995c2b69852e2dc6c3d946b6edd5f1b1afb4b5
zadragon <mase0804@naver.com>
2022-04-10 22:42:02 +0900

    update

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,53 +1,66 @@
-import {useState} from "react";
-import {FieldErrors, useForm} from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
 
 interface LoginForm {
     username: string;
     password: string;
     email: string;
+    errors?: string;
 }
 
 export default function Forms() {
-    const {register, handleSubmit} = useForm<LoginForm>();
-    const onValid = (data:LoginForm) => {
-
-        console.log('inm here')
-    }
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        watch,
+        setError,
+        setValue,
+        reset,
+        resetField,
+    } = useForm<LoginForm>({
+        mode: "onChange",
+    });
+    const onValid = (data: LoginForm) => {
+        console.log("im valid bby");
+    };
     const onInvalid = (errors: FieldErrors) => {
-        console.log(errors)
-    }
+        console.log(errors);
+    };
+
+    setValue("username","hice")
     return (
         <form onSubmit={handleSubmit(onValid, onInvalid)}>
             <input
                 {...register("username", {
                     required: "Username is required",
                     minLength: {
-                        message:"the username should be longer than 5 chars",
-                        value:5
-                    }
+                        message: "The username should be longer than 5 chars.",
+                        value: 5,
+                    },
                 })}
                 type="text"
                 placeholder="Username"
-                required
             />
+            {errors.username?.message}
             <input
                 {...register("email", {
-                    required: true
+                    required: "Email is required",
+                    validate: {
+                        notGmail: (value) =>
+                            !value.includes("@gmail.com") || "Gmail is not allowed",
+                    },
                 })}
                 type="email"
                 placeholder="Email"
-                required
             />
-
+            {errors.email?.message}
             <input
-                {...register("password", {
-                    required: true
-                })}
+                {...register("password", { required: "Password is required" })}
                 type="password"
                 placeholder="Password"
-                required
             />
-            <input type="submit" value="Create Account"/>
+            <input type="submit" value="Create Account" />
+            {errors.errors?.message}
         </form>
     );
 }
