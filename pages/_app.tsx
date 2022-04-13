f08c852826624de12d69e5bc82447eb285cfe73f
zadragon <mase0804@naver.com>
2022-04-13 23:10:12 +0900

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,5 +4,7 @@ import type { AppProps } from 'next/app'
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+// 프리즈마 연결
+//pscale connect carrot-market
 
 export default MyApp
