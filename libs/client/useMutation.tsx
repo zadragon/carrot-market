80e8437c6e978a3d029a3567a27ef41bc298f0c8
zadragon <mase0804@naver.com>
2022-05-02 23:21:19 +0900

    update

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -1,34 +1,33 @@
-import {useState} from "react";
 
-export default function useMutation(url: string): [
-    (data?: any) => void,
-    { loading: boolean; data: undefined | any; error: undefined | any }
-] {
 
-    const [state, setState] = useState({
-        loading:false,
-        data:undefined,
-        error:undefined
-    })
+import { useState } from "react";
 
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState<undefined | any>(undefined);
-    const [error, setError] = useState<undefined | any>(undefined);
+interface UseMutationState {
+    loading: boolean;
+    data?: object;
+    error?: object;
+}
+type UseMutationResult = [(data: any) => void, UseMutationState];
 
+export default function useMutation(url: string): UseMutationResult {
+    const [state, setSate] = useState<UseMutationState>({
+        loading: false,
+        data: undefined,
+        error: undefined,
+    });
     function mutation(data: any) {
-        setLoading(true);
+        setSate((prev) => ({ ...prev, loading: true }));
         fetch(url, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(data),
         })
-            .then((response) => response.json().catch(()=>{}))
-            .then(setData)
-            .catch(setError)
-            .finally(() => setLoading(false));
+            .then((response) => response.json().catch(() => {}))
+            .then((data) => setSate((prev) => ({ ...prev, data })))
+            .catch((error) => setSate((prev) => ({ ...prev, error })))
+            .finally(() => setSate((prev) => ({ ...prev, loading: false })));
     }
-
-    return [mutation, {loading, data, error}];
+    return [mutation, { ...state }];
 }
--- pages/api/user/enter.tsx
+++ pages/api/user/enter.tsx
@@ -1,24 +1,75 @@
+import mail from "@sendgrid/mail";
+import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { phone, email } = req.body;
-    const payload = phone ? { phone: +phone } : { email };
-    const user = await client.user.upsert({
-        where: {
-            ...payload,
-        },
-        create: {
-            name: "Anonymous",
-            ...payload,
-        },
-        update: {},
-    });
-    console.log(user);
 
-    return res.status(200).end();
-    //
+mail.setApiKey(process.env.SENDGRID_KEY!);
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GMAIL_ID,
+        pass: process.env.GMAIL_PWD,
+    },
+});
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseType>
+) {
+    const { phone, email } = req.body;
+    const user = phone ? { phone: +phone } : email ? { email } : null;
+    if (!user) return res.status(400).json({ ok: false });
+    const payload = Math.floor(100000 + Math.random() * 900000) + "";
+    const token = await client.token.create({
+        data: {
+            payload,
+            user: {
+                connectOrCreate: {
+                    where: {
+                        ...user,
+                    },
+                    create: {
+                        name: "Anonymous",
+                        ...user,
+                    },
+                },
+            },
+        },
+    });
+    if (phone) {
+        const message = await twilioClient.messages.create({
+            messagingServiceSid: process.env.TWILIO_MSID,
+            to: process.env.MY_PHONE!,
+            body: `Your login token is ${payload}.`,
+        });
+        console.log(message);
+    }else if (email) {
+        const sendEmail = await transporter.sendMail({
+            from: `ABC <thurpia01@gmail.com>`,
+            to: email,
+            subject: 'token',
+            text: `your login token is ${payload}`,
+            html: `
+          <div style="text-align: center;">
+            <h3 style="color: #FA5882">ABC</h3>
+            <br />
+            <p>your login token is ${payload}</p>
+          </div>
+      `})
+            .then((result: any) => console.log(result))
+            .catch((err: any) => console.log(err))
+    }
+
+    return res.json({
+        ok: true,
+    });
 }
 
 export default withHandler("POST", handler);
